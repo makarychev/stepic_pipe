e3586cd396a66078fb917fba058fbd90f716ad97
makarychev <mmakarychev@yahoo.com>
2016-02-02 23:22:11 +0200

    2 pipe example

--- source.cpp
+++ source.cpp
@@ -9,23 +9,41 @@ void who_wc()
     int pipe2[2];
     int pipe3[2];
     pipe(pipe1);
+
     if (!fork())
     {
-        close(STDOUT_FILENO);
-        dup2(pipe1[1], STDOUT_FILENO);
-        close(pipe1[0]);
-        close(pipe1[1]);
-        execlp("ls", "ls", NULL);
+        if (!fork())
+        {
+            close(STDOUT_FILENO);
+            dup2(pipe1[1], STDOUT_FILENO);
+            close(pipe1[0]);
+            close(pipe1[1]);
+            close(pipe2[0]);
+            close(pipe2[1]);
+            execlp("ls", "ls", NULL);
+        }
+        else
+        {
+            close(STDOUT_FILENO);
+            close(STDIN_FILENO);
+            dup2(pipe1[0], STDIN_FILENO);
+            dup2(pipe2[1], STDOUT_FILENO);
+            close(pipe1[0]);
+            close(pipe1[1]);
+            close(pipe2[0]);
+            close(pipe2[1]);
+            execlp("grep", "grep", "sour*", NULL);
+        }
     } else
     {
         close(STDIN_FILENO);
-        dup2(pipe1[0], STDIN_FILENO);
+        dup2(pipe2[0], STDIN_FILENO);
         close(pipe1[0]);
         close(pipe1[1]);
+        close(pipe2[0]);
+        close(pipe2[1]);
         execlp("wc", "wc", "-l", NULL);
     }
-    wait(NULL);
-    wait(NULL);
 }
 
 int main (int argc, char ** argv)
