1ecb663ad541a2329775964d51a7cba9fbd2e3e3
Maksym Makarychev <Maksym Makarychev>
2016-02-10 13:28:58 +0200

    fix: bad file name in ftok()

--- shm_sysV.cpp
+++ shm_sysV.cpp
@@ -3,35 +3,28 @@
 #include <sys/shm.h>
 #include <stdio.h>
 #include <string.h>
-#include <iostream>
-
-using namespace std;
-
 
 void test_shmSysV()
 {
-    int size = 16; // 2024*1024
-    key_t key = ftok("//tmp//mem.tmp", 1);
+    int size = 1024*1024; //
+    key_t key = ftok("/tmp/mem.temp", 1);
     int shm_id = shmget(key, size, IPC_CREAT | 0777);
     if (shm_id < 0) {
             printf("shmget error = %d\n", shm_id);
             return;
     }
-    void *data = shmat(shm_id, (void *)0, 0);
+    void *data = shmat(shm_id, 0, 0);
     if (data == (char *)-1)
     {
             printf("shmat error\n");
             return;
     }
-    char str[17] = {0};
-    cout << "data = \n" << (char* )memcpy(str, (char *)data, size) << endl;
-    char c = 31;
-    void* p = memset(data, c, size);
-    cout << "p = \n" << (char* )memcpy(str, (char *)p, size) << endl;
-}
+    char c = 42;
+     void* p = memset(data, c, size);
+ }
 
-int main(int argc, char* argv[])
-{
+ int main(int argc, char* argv[])
+ {
     test_shmSysV();
 
     return 0;
